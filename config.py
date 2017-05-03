e58ca262cc6782f174fd0a0abdfbe093fcb88610
Diego Silva Pires <diego_silva_pires@outlook.com>
2017-05-02 21:11:24 -0400

    first commit

---
+++ config.py
@@ -0,0 +1,31 @@
+# config.py
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    """
+    Common configurations
+    """
+    # Put any configurations here that are common across all environments
+
+class DevelopmentConfig(Config):
+    """
+    Development configurations
+    """
+    # http://flask.pocoo.org/docs/0.11/config/
+    # http://flask-sqlalchemy.pocoo.org/2.1/config/
+
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+
+    DEBUG = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
