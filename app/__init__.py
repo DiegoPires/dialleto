e2102639cd12d5702cd05475edb468fd097b2027
Diego Silva Pires <diego_silva_pires@outlook.com>
2017-05-09 21:03:02 -0400

    Added initial database

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@
 # third-party imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
 
 # local imports
 from config import app_config
@@ -10,15 +12,23 @@ from config import app_config
 # db variable initialization
 db = SQLAlchemy()
 
+login_manager = LoginManager()
+
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
-    # temporary route
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
+    # authentication config
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
+
+    migrate = Migrate(app, db)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from app import models
 
     return app
